7dcc0be6d0ebf13d62417aa688ee994bce256a0f
kalinovdv <kdv_ul@rambler.ru>
2021-08-18 08:27:53 +0400

    Добавил во ViewModel рандомизатор для получения случайного результата работы модели.

--- app/src/main/java/ru/geekbrains/myweather/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/geekbrains/myweather/viewmodel/MainViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.geekbrains.myweather.model.RepositoryImpl
-import java.lang.Thread.sleep
 
 class MainViewModel(private val liveDataToObserve : MutableLiveData<Any> = MutableLiveData(),
                     private val repositoryImpl: RepositoryImpl = RepositoryImpl()
@@ -21,11 +20,17 @@ class MainViewModel(private val liveDataToObserve : MutableLiveData<Any> = Mutab
     }
 
     private fun getDataFromLocalSource() {
-        liveDataToObserve.value = AppState.Loading
+        val stateRandom = (0..4).random()
+        when (stateRandom){
+            1 -> liveDataToObserve.value = AppState.Loading
+            2 -> liveDataToObserve.value = AppState.Seccess(repositoryImpl.getWeatherFromLocalStorage())
+            3 -> {/*обработка ошибок*/}
+        }
+        /*liveDataToObserve.value = AppState.Loading
         Thread{
             sleep(3000)
             liveDataToObserve.postValue(AppState.Seccess(repositoryImpl.getWeatherFromLocalStorage()))
-        }.start()
+        }.start()*/
     }
 
 }
