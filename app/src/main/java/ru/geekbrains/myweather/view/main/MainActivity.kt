5dd2871debf0212d956867deb8506c208553a0dc
kalinovdv <kdv_ul@rambler.ru>
2021-08-19 23:35:20 +0400

    Добавил MainFragment.kt и MainFragmentAdapter.kt для вывода списка городов.

--- app/src/main/java/ru/geekbrains/myweather/view/main/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/myweather/view/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package ru.geekbrains.myweather.view
+package ru.geekbrains.myweather.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, DetailsFragment.newInstance())
+                .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
     }
---
+++ app/src/main/java/ru/geekbrains/myweather/view/main/MainFragment.kt
@@ -0,0 +1,77 @@
+package ru.geekbrains.myweather.view.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
+import ru.geekbrains.myweather.R
+import ru.geekbrains.myweather.databinding.FragmentMainBinding
+import ru.geekbrains.myweather.viewmodel.AppState
+import ru.geekbrains.myweather.viewmodel.MainViewModel
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding
+    private lateinit var viewModel: MainViewModel
+    private val adapter = MainFragmentAdapter()
+    private var isDataSetRus: Boolean = true
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.mainFragmentRecyclerView?.adapter = adapter
+        binding?.mainFragmentFAB?.setOnClickListener { changeWeatherDataSet() }
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.getLiveData().observe(viewLifecycleOwner, { renderData(it as AppState) })
+        viewModel.getWeatherFromLocalSourceRus()
+    }
+
+    private fun renderData(appState: AppState){
+        when(appState) {
+            is AppState.Loading -> {
+                binding?.mainFragmentLoadingLayout?.visibility = View.VISIBLE
+            }
+            is AppState.Seccess -> {
+                binding?.mainFragmentLoadingLayout?.visibility = View.GONE
+                adapter.setWeather(appState.weatherData)
+            }
+            is AppState.Error -> {
+                binding?.mainFragmentLoadingLayout?.visibility = View.GONE
+                Snackbar
+                    .make(binding?.mainFragmentFAB!!, getString(R.string.error),
+                        Snackbar.LENGTH_INDEFINITE)
+                    .setAction(getString(R.string.reload)) {
+                        viewModel.getWeatherFromLocalSourceRus() }
+                    .show()
+            }
+        }
+    }
+
+    private fun changeWeatherDataSet() {
+        if (isDataSetRus) {
+            viewModel.getWeatherFromLocalSourceWorld()
+            binding?.mainFragmentFAB?.setImageResource(R.drawable.ic_earth)
+        } else {
+            viewModel.getWeatherFromLocalSourceRus()
+            binding?.mainFragmentFAB?.setImageResource(R.drawable.ic_russia)
+        }
+        isDataSetRus = !isDataSetRus
+    }
+
+        companion object {
+            fun newInstance() = MainFragment()
+        }
+
+}
