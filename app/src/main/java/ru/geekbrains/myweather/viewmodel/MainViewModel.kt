0a08fb2acf700ca0cc3c72623ab591e433e118a0
kalinovdv <kdv_ul@rambler.ru>
2021-08-24 00:11:00 +0400

    Отформатировал и убрал лишние импорты в классе MainViewModel.kt.

--- app/src/main/java/ru/geekbrains/myweather/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/geekbrains/myweather/viewmodel/MainViewModel.kt
@@ -1,13 +1,13 @@
 package ru.geekbrains.myweather.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.geekbrains.myweather.model.RepositoryImpl
 import java.lang.Thread.sleep
 
-class MainViewModel(private val liveDataToObserve : MutableLiveData<Any> = MutableLiveData(),
-                    private val repositoryImpl: RepositoryImpl = RepositoryImpl()
+class MainViewModel(
+    private val liveDataToObserve: MutableLiveData<Any> = MutableLiveData(),
+    private val repositoryImpl: RepositoryImpl = RepositoryImpl()
 ) : ViewModel() {
 
     fun getLiveData() = liveDataToObserve
@@ -16,18 +16,16 @@ class MainViewModel(private val liveDataToObserve : MutableLiveData<Any> = Mutab
     fun getWeatherFromLocalSourceWorld() = getDataFromLocalSource(false)
     fun getWeatherFromRemoteSource() = getDataFromLocalSource(true)
 
-    /*fun getData() : LiveData<Any> {
-        getDataFromLocalSource()
-        return liveDataToObserve
-    }*/
-
-    private fun getDataFromLocalSource(isRussian : Boolean) {
+    private fun getDataFromLocalSource(isRussian: Boolean) {
         liveDataToObserve.value = AppState.Loading
-        Thread{
+        Thread {
             sleep(2000)
-            liveDataToObserve.postValue(AppState.Seccess(if (isRussian) repositoryImpl.getWeatherFromLocalStorageRus()
-                                                            else repositoryImpl.getWeatherFromLocalStorageWorld()))
+            liveDataToObserve.postValue(
+                AppState.Seccess(
+                    if (isRussian) repositoryImpl.getWeatherFromLocalStorageRus()
+                    else repositoryImpl.getWeatherFromLocalStorageWorld()
+                )
+            )
         }.start()
     }
-
 }
