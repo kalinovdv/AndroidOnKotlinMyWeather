4424985078604af1baec3be96923b678f282c10b
kalinovdv <kdv_ul@rambler.ru>
2021-08-26 17:08:41 +0400

    Создал класс WeatherDTO.kt для данных, получаемых с сервера.

---
+++ app/src/main/java/ru/geekbrains/myweather/model/WeatherDTO.kt
@@ -0,0 +1,12 @@
+package ru.geekbrains.myweather.model
+
+data class WeatherDTO(
+    val fact: FactDTO?
+)
+
+data class FactDTO(
+    val temp: Int?,
+    val feels_like: Int?,
+    val condition: String
+)
+
