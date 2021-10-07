bae0f31909e3cd16a0018b38ebced6f7340be118
kalinovdv <kdv_ul@rambler.ru>
2021-10-07 22:50:10 +0400

    Создал базу данных через паттерн Singleton.

--- app/src/main/java/ru/geekbrains/myweather/room/HistoryDataBase.kt
+++ app/src/main/java/ru/geekbrains/myweather/room/HistoryDataBase.kt
@@ -1,8 +1,9 @@
 package ru.geekbrains.myweather.room
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = arrayOf(HistoryEntity::class), version = 1, exportSchema = false)
-abstract class HistoryDataBase {
+abstract class HistoryDataBase : RoomDatabase() {
     abstract fun historyDao(): HistoryDao
 }
