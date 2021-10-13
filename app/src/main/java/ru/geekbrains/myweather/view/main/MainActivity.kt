0ae1db74173075be28ac83d80bb60c1995372aa5
kalinovdv <kdv_ul@rambler.ru>
2021-10-13 13:02:43 +0400

    Добавил адаптер и обработку фрагмента для просмотра истории запросов.
    В MainActivity добавил вывод меню "История"

--- app/src/main/java/ru/geekbrains/myweather/view/main/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/myweather/view/main/MainActivity.kt
@@ -1,9 +1,12 @@
 package ru.geekbrains.myweather.view.main
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import ru.geekbrains.myweather.R
 import ru.geekbrains.myweather.databinding.MainActivityBinding
+import ru.geekbrains.myweather.view.history.HistoryFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +14,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = MainActivityBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
         setContentView(R.layout.main_activity)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
@@ -21,4 +27,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_screen_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_history -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                        .add(R.id.container, HistoryFragment.newInstance())
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
