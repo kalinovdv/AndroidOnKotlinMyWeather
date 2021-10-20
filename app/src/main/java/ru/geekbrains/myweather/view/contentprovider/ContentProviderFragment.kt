97cadfb6df81ffa0e5c614be95ea72ad3e8115eb
kalinovdv <kdv_ul@rambler.ru>
2021-10-20 23:50:32 +0400

    Добавил фрагмент для отображения списка контактов.
    Добавил пункт меню для вызова списка контактов и обработчик нажатия на пунк меню.

---
+++ app/src/main/java/ru/geekbrains/myweather/view/contentprovider/ContentProviderFragment.kt
@@ -0,0 +1,97 @@
+package ru.geekbrains.myweather.view.contentprovider
+
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import ru.geekbrains.myweather.databinding.FragmentContentProviderBinding
+
+const val REQUES_CODE = 42
+
+class ContentProviderFragment : Fragment() {
+
+    private var _binding: FragmentContentProviderBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentContentProviderBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ContentProviderFragment()
+    }
+
+    private fun checkPermission() {
+        context?.let {
+            when {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    android.Manifest.permission.READ_CONTACTS
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    //getContacts()
+                }
+
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_CONTACTS) -> {
+                    AlertDialog.Builder(it)
+                        .setTitle("Доступ к контактам")
+                        .setMessage("Объяснение")
+                        .setPositiveButton("Представить доступ") { _, _ -> requestPermission() }
+                        .setNegativeButton("Не надо") { dialog, _ -> dialog.dismiss() }
+                        .create()
+                        .show()
+                }
+                else -> {
+                    requestPermission()
+                }
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        requestPermissions(arrayOf(android.Manifest.permission.READ_CONTACTS), REQUES_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUES_CODE -> {
+                if ((grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    //getContacts()
+                } else {
+                    context?.let {
+                        AlertDialog.Builder(it)
+                            .setTitle("Доступ к контактам")
+                            .setMessage("Объяснение")
+                            .setNegativeButton("Закрыть") { dialog, _ -> dialog.dismiss() }
+                            .create()
+                            .show()
+                    }
+                }
+                return
+            }
+        }
+    }
+}
--- app/src/main/java/ru/geekbrains/myweather/view/main/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/myweather/view/main/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import ru.geekbrains.myweather.R
 import ru.geekbrains.myweather.databinding.MainActivityBinding
+import ru.geekbrains.myweather.view.contentprovider.ContentProviderFragment
 import ru.geekbrains.myweather.view.history.HistoryFragment
 
 class MainActivity : AppCompatActivity() {
@@ -38,6 +39,15 @@ class MainActivity : AppCompatActivity() {
                 supportFragmentManager.apply {
                     beginTransaction()
                         .add(R.id.container, HistoryFragment.newInstance())
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+                true
+            }
+            R.id.menu_content_provider -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                        .add(R.id.container, ContentProviderFragment.newInstance())
                         .addToBackStack("")
                         .commitAllowingStateLoss()
                 }
